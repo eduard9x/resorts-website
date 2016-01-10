0c2d78b07c7c6b8aa7adbbe42ee3ac167cd2536e
eduard9x <ghinea.eduard@yahoo.com>
2016-01-10 20:26:45 +0000

    fixed search

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,13 +1,14 @@
 $.getJSON('data.json', function(data) {
   $(document).ready(function(){
 
-
       display();
 
       //function for the date picker
-      $(function() {
-        $(".datepicker").datepicker();
-      });
+
+        $(".datepicker").datepicker({dateFormat: 'yy-mm-dd'});
+
+
+
 
       //function for the select menu
       $(function() {
@@ -17,23 +18,115 @@ $.getJSON('data.json', function(data) {
         $("#resortPrice").selectmenu();
       });
 
+
+
       function display(){
-          var output="<ul class='columns'>";
-          var userDest = $("input:checked").val();
-          if(userDest != undefined){
-            for (var i in data.resorts) {
-                if((userDest == data.resorts[i].destination) || (userDest == "Any"))
-                    output+="<li><img src='" + data.resorts[i].picture + "'><h2><a href='" + data.resorts[i].url + "'>" + data.resorts[i].name + "</a></h2><h4> Available between " + data.resorts[i].startDate + " and " + data.resorts[i].endDate + "</h4><h4>Price per night: £"
-                     + data.resorts[i].price + "</h4><h4>" + data.resorts[i].short_description + "</h4>" +
-                      "</li>";
+
+            var output = "" ;
+            var showResort = true;
+
+            for (var i=0;i<data.resorts.length;i++){
+
+              if(document.getElementById("resortDestination").value != data.resorts[i].destination && document.getElementById("resortDestination").value != "Any") showResort = false;
+              if(document.getElementById("comfortLevel").value != data.resorts[i].comfortLevel && document.getElementById("comfortLevel").value != "Any") showResort = false;
+
+              if(showResort==true && document.getElementById("resortActivities").value == "Any");
+              else if(document.getElementById("resortActivities").value != "Any"){
+                      var numberOfActivities = data.resorts[i].activities.length;
+                      var found = false;
+                      for (var j=0;j<numberOfActivities;j++){
+                      if(document.getElementById("resortActivities").value == data.resorts[i].activities[j]) found = true;
+                      }
+                      if(found==false) showResort=false;
+              }
+
+
+              if(showResort==true && document.getElementById("resortPrice").value == "Any");
+              else if(document.getElementById("resortPrice").value != "Any")
+
+                    if(document.getElementById("resortPrice").value=="less than £50"){
+                      if(data.resorts[i].price >= 50) showResort = false;
+                    }
+                    else if(document.getElementById("resortPrice").value=="between £50 - £100"){
+                      if(data.resorts[i].price < 50 || data.resorts[i].price > 100) showResort = false;
+                    }
+                    else if(document.getElementById("resortPrice").value=="more than £100")
+                      if(data.resorts[i].price <= 100) showResort = false;
+
+                if(showResort==true && document.getElementById("StartDate").value.length == 0);
+                else if(document.getElementById("StartDate").value.length == 10){
+
+                  var chosenStartDate = document.getElementById("StartDate").value;
+                  var jsonEndDate = data.resorts[i].endDate;
+                  var jsonStartDate = data.resorts[i].startDate;
+
+                  if(chosenStartDate < jsonStartDate || chosenStartDate > jsonEndDate) {
+                    showResort = false;
+                }
+              }
+
+              if(showResort==true && document.getElementById("EndDate").value.length == 0);
+              else if(document.getElementById("EndDate").value.length == 10){
+
+                var chosenEndDate = document.getElementById("EndDate").value;
+                var jsonEndDate = data.resorts[i].endDate;
+                var jsonStartDate = data.resorts[i].startDate;
+
+                if(chosenEndDate < jsonStartDate || chosenEndDate > jsonEndDate) {
+                  showResort = false;
+              }
+              }
+
+              if(showResort==true) {
+                output+="<ul class='columns'><li><img src='" + data.resorts[i].picture + "'><h2><a href='" + data.resorts[i].url + "'>" + data.resorts[i].name + "</a></h2><h4>" + data.resorts[i].startDate + " to " + data.resorts[i].endDate + "</h4><h4>Price per night: £" + data.resorts[i].price + "</h4><h4>" + data.resorts[i].short_description + "</h4>" + "</li></ul>";
+              }
+              showResort = true;
             }
-          output+="</ul>";
-          }
-          else {
-            output= "<h4>Please select an option first.</h4>";
-          }
-          document.getElementById("placeholder").innerHTML=output;
-      }
+
+            if(output.length==0) output = "<h2> Sorry. No results found. </h2> "
+
+              document.getElementById("placeholder").innerHTML=output;
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      // function display(){
+      //     var output="<ul class='columns'>";
+      //     var userDest = $("input:checked").val();
+      //     if(userDest != undefined){
+      //       for (var i in data.resorts) {
+      //           if((userDest == data.resorts[i].destination) || (userDest == "Any"))
+      //               output+="<li><img src='" + data.resorts[i].picture + "'><h2><a href='" + data.resorts[i].url + "'>" + data.resorts[i].name + "</a></h2><h4>" + data.resorts[i].startDate + " to " + data.resorts[i].endDate + "</h4><h4>Price per night: £"
+      //                + data.resorts[i].price + "</h4><h4>" + data.resorts[i].short_description + "</h4>" +
+      //                 "</li>";
+      //       }
+      //     output+="</ul>";
+      //     }
+      //     else {
+      //       output= "<h4>Please select an option first.</h4>";
+      //     }
+      //
+      //     document.getElementById("placeholder").innerHTML=output;
+      // }
 
       $("#search").on("click", function(){
           display();
